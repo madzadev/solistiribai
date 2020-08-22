4e7fa9ba1795cfa2bbc7b72b97a647193a00f692
madarsbiss <madars.biss@gmail.com>
2020-08-22 23:46:28 +0300

    added about-us fb feedback

--- src/pages/par-mums.js
+++ src/pages/par-mums.js
@@ -12,8 +12,15 @@ const aboutUsPage = () => {
     <Layout>
       <Helmet>
         <title>Par mums | Solis Tīrībai</title>
+        <script
+          async
+          defer
+          crossorigin="anonymous"
+          src="https://connect.facebook.net/lv_LV/sdk.js#xfbml=1&version=v8.0&appId=620075488933384&autoLogAppEvents=1"
+          nonce="i1jpVPSm"
+        ></script>
       </Helmet>
-
+      <div id="fb-root"></div>
       <div className="about-content">
         <div className="about-why">
           <h1 className="about-title">Kāpēc izvēlēties mūs?</h1>
@@ -25,10 +32,10 @@ const aboutUsPage = () => {
             <img src={Tick} alt="" />
             <p>Individuāla pieeja katram klientam</p>
           </div>
-          <div className="about-reason">
+          {/* <div className="about-reason">
             <img src={Tick} alt="" />
             <p>Savu darbu izpildām rūpīgi un atbildīgi</p>
-          </div>
+          </div> */}
 
           <div className="about-reason">
             <img src={Tick} alt="" />
@@ -61,6 +68,20 @@ const aboutUsPage = () => {
         </div>
         <div className="about-image">
           {/* <img className="aboutImg" src={aboutImage} alt="img" /> */}
+          {/* <h1 className="about-title">Atsauksmes</h1> */}
+          <iframe
+            src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fineta.dzerina1%2Fposts%2F1767488250055287&show_text=true&width=auto&appId=620075488933384&height=175"
+            style={{
+              border: "none",
+              overflow: "hidden",
+              width: "100%",
+              height: "175px",
+            }}
+            scrolling="no"
+            frameborder="0"
+            allowTransparency="true"
+            allow="encrypted-media"
+          ></iframe>
         </div>
       </div>
 
