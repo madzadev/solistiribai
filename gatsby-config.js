a8d006fe807d07378d00ad6dda72d838d4ddcd7a
madarsbiss <madars.biss@gmail.com>
2020-08-26 22:56:02 +0300

    added sitemap in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,14 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-sass`, `gatsby-plugin-react-helmet`],
+  // siteMetadata: {
+  //   siteUrl: `https://www.solistiribai.netlify.appa`,
+  // },
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-react-helmet`,
+    // `gatsby-plugin-sitemap`,
+  ],
   proxy: {
     prefix: "/contact",
     url: "https://solistiribai.herokuapp.com",
